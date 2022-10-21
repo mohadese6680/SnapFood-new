c322f0d4c81b19ca1747bc78053e070bb42eaa4f
mohadese <mohadesehashemi6680@gmail.com>
2022-10-21 19:52:09 +0330

    this is the first commit

---
+++ app.js
@@ -0,0 +1,17 @@
+// let button = document.querySelector(".button--popup");
+// let pop = document.querySelector(".popup--div");
+
+// button.onclick = showMenu;
+
+// function showMenu() {
+//     pop.classList.toggle("opener");
+// }
+
+// let buton = document.querySelector(".popup--close");
+// let popup = document.querySelector(".popup--div");
+
+// buton.onclick = closeMenu;
+
+// function closeMenu() {
+//     popup.classList.toggle("closer");
+// }
